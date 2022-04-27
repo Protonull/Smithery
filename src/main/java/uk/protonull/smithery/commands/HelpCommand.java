bfb87778f4202a221f5d7c4fa68f7b9306b6823e
Alexander <protonull@protonmail.com>
2022-04-27 04:23:47 +0100

    Update "/smithery help" description

--- src/main/java/uk/protonull/smithery/commands/HelpCommand.java
+++ src/main/java/uk/protonull/smithery/commands/HelpCommand.java
@@ -2,12 +2,16 @@ package uk.protonull.smithery.commands;
 
 import co.aikar.commands.CommandHelp;
 import co.aikar.commands.annotation.CommandAlias;
+import co.aikar.commands.annotation.Description;
+import co.aikar.commands.annotation.Syntax;
 import org.bukkit.command.CommandSender;
 import vg.civcraft.mc.civmodcore.command.AikarCommand;
 
 @CommandAlias(CommandRegistrar.ROOT_COMMAND_ALIAS)
 public final class HelpCommand extends AikarCommand {
 
+    @Description("Shows all Smithery commands")
+    @Syntax("")
     @co.aikar.commands.annotation.HelpCommand
     public void help(final CommandSender sender,
                      final CommandHelp help) {
